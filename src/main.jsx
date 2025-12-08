317c45bb68bba118604f7ca446cb4ec203d59705
Saraswathi <saraswathiachari02@gmail.com>
2025-12-08 12:28:36 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./App.jsx";
+import App from "./App";
+import { CartProvider } from "./context/CartContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </HashRouter>
   </React.StrictMode>
 );
